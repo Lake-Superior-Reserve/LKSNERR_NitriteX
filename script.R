a3658a9fb58c4277e6054ffa94d51a52479f9fa2
hnramage <hannah.ramage@wisc.edu>
2024-02-14 16:37:49 -0600

    Add existing project files to Git

--- script.R
+++ script.R
@@ -3,7 +3,7 @@ library("pacman")
 
 # packages commonly needed for SWMP data
 # checks to see if a package is installed, if not it attempts to install the package and then loads it
-pacman::p_load(SWMPr, SWMPrExtension, lubridate, tidyverse, scales, extrafont, cowplot, ggThemeAssist, gridExtra)
+pacman::p_load(SWMPr, SWMPrExtension, lubridate, tidyverse)
 
 ##Feed in our BA Nut data
 barkers <- import_local("./data", "lksbanut", trace = TRUE)
